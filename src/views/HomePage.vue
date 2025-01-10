4c7072a8a4004c944ca40ecca33686a734e1c626
Jetross <jetrossneri07@gmail.com>
2025-01-10 09:17:55 +0800

    added vuetify and applied mdi icons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-responsive class="border rounded" max-height="300">
+  <v-responsive class="border rounded">
     <v-app :theme="theme">
       <v-app-bar class="px-3">
         <v-spacer></v-spacer>
 
         <v-btn
           :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
           slim
           @click="onClick"
+          rounded="xl"
+          elevation="6"
         ></v-btn>
       </v-app-bar>
 
       <v-main>
         <v-container>
-          <h1>Main Content</h1>
+          <h1>Welcome to Bloglist</h1>
         </v-container>
       </v-main>
     </v-app>
@@ -30,3 +31,10 @@ function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
 </script>
+
+<style>
+h1 {
+  margin-top: 10%;
+  text-align: center;
+}
+</style>
